8efebdde198c151be4c661a518182713ea8a4076
Julian Zucker <julian.zucker@gmail.com>
2019-07-09 07:33:03 -0600

    Update pom to include evvo

--- src/main/scala/io/evvo/App.scala
+++ src/main/scala/io/evvo/App.scala
@@ -6,7 +6,9 @@ import io.evvo.island.population.{Minimize, Objective}
 object App {
 
   class TestObj extends Objective[Double]("test", Minimize) {
-    override protected def objective(sol: Double): Double = ???
+    override protected def objective(sol: Double): Double = {
+      3
+    }
   }
 
   def main(args : Array[String]) {
