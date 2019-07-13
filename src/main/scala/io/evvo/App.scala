553a6442626e5548b111c8790de1fdcc3a016f01
Julian Zucker <julian.zucker@gmail.com>
2019-07-13 07:36:46 -0600

    Use local version of evvo for development

--- src/main/scala/io/evvo/App.scala
+++ src/main/scala/io/evvo/App.scala
@@ -1,9 +1,13 @@
 package io.evvo
 
+import io.evvo.agent.defaults.trees.ChangeLeafDataModifier
 import io.evvo.island.population.{Minimize, Objective}
 
 
 object App {
+
+  val mod = ChangeLeafDataModifier[Boolean, Int](_ + 1)
+
 
   class TestObj extends Objective[Double]("test", Minimize) {
     override protected def objective(sol: Double): Double = {
