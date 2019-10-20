ce2d6b587b8f58e04acc68acc96ab100784ca57d
Julian Zucker <julian.zucker@gmail.com>
2019-10-20 14:39:10 -0600

    Add adult income dataset

--- data/script/generate_datafiles.py
+++ data/script/generate_datafiles.py
@@ -5,14 +5,14 @@ and writes the results to `./test/` and `./train/`.
 Invoke with `./data/script/generate_datafiles.py` from the top level."""
 
 import csv
-# The idea is that we can write each data-file processing script separately, and import
-# and call them here to make re-generation easy.
 import os
 import random
 
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
 
 def main():
-    os.makedirs("data/raw/", exist_ok=True)
     os.makedirs("data/test", exist_ok=True)
     os.makedirs("data/train", exist_ok=True)
     random.seed(87333)
@@ -27,7 +27,6 @@ def main():
         labels = [bool_to_outcome(row[-1] == "1") for row in data]
         # Ninth column is gender + marital status, 1,3,4 correspond to maleness
         gender = [row[8] in ["1", "3", "4"] for row in data]
-
         write_data("german", features, labels, gender)
 
     ###############################################################################################
@@ -47,9 +46,44 @@ def main():
 
         write_data("compas", features, labels, priv)
 
+    ###############################################################################################
+    # Adult income dataset
+    with open("data/raw/adult_income.csv") as data_reading_fd:
+        _ = data_reading_fd.readline()  # Skip header
+        data = [[item.strip() for item in row]
+                for row in csv.reader(data_reading_fd, delimiter=",")]
+        random.shuffle(data)
+
+        t = np.transpose(data)
+        result = []
+        gender_encoder = None
+        priv_encoder = None
+        for col in t:
+            try:
+                result.append([int(x) for x in col])
+            except:
+                le = LabelEncoder().fit(col)
+
+                if col[0] in ["Male", "Female"]:
+                    gender_encoder = le
+
+                if col[0] in ["<=50K", ">50K"]:
+                    priv_encoder = le
+
+                result.append(le.transform(col))
+
+        result = np.transpose(result)
+
+        labels = [bool_to_outcome(row[-1] == priv_encoder.transform([">50K"])[0])
+                  for row in result]
+        priv = [row[9] == gender_encoder.transform(["Male"])[0] for row in result]
+        result = np.delete(result, -1, 1)
+
+        write_data("adult_income", result, labels, priv)
+
 
 def write_data(filename, features, labels, priv):
-    train_proportion = .8
+    train_proportion = .7
     train_cutoff = int(len(features) * train_proportion)
 
     train_features = features[0:train_cutoff]
---
+++ data/script/run_benchmarks.py
@@ -0,0 +1,68 @@
+import csv
+
+import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.tree import DecisionTreeClassifier
+
+
+def main():
+    np.random.seed(8799)
+    for filename in ["german", "COMPAS", "adult_income"]:
+        run_benchmark(filename)
+
+
+def run_benchmark(filename):
+    print("=====================")
+    print(filename)
+    with open(f"data/train/{filename}.data", "r") as train_feature_file:
+        train_features = list(csv.reader(train_feature_file))
+
+    with open(f"data/train/{filename}.labels", "r") as train_label_file:
+        train_labels = [prediction.strip() == "Positive" for prediction in
+                        train_label_file.readlines()]
+
+    model = RandomizedSearchCV(DecisionTreeClassifier(),
+                               {
+                                   "min_samples_split": np.arange(2, 10),
+                                   "min_samples_leaf": np.arange(2, 10),
+                                   "max_depth": np.arange(1, 10),
+                                   "min_impurity_decrease": np.arange(0.0, 0.5)
+                               }, cv=3)
+    model.fit(train_features, train_labels)
+
+    with open(f"data/test/{filename}.data", "r") as test_feature_file:
+        test_features = list(csv.reader(test_feature_file))
+
+    with open(f"data/test/{filename}.labels", "r") as test_label_file:
+        test_labels = [prediction.strip() == "Positive" for prediction in
+                       test_label_file.readlines()]
+
+    with open(f"data/test/{filename}.priv", "r") as test_priv_file:
+        test_priv = [priv.strip() == "True" for priv in test_priv_file]
+
+    predictions = model.predict(test_features)
+    accuracy = np.mean(np.equal(predictions, test_labels))
+
+    priv = []
+    priv_labels = []
+    nonpriv = []
+    nonpriv_labels = []
+    for datapoint, label, datapoint_priv in zip(test_features, test_labels, test_priv):
+        if datapoint_priv:
+            priv.append(datapoint)
+            priv_labels.append(label)
+        else:
+            nonpriv.append(datapoint)
+            nonpriv_labels.append(label)
+
+    priv_pos = np.mean(model.predict(priv))
+    non_priv_pos = np.mean(model.predict(nonpriv))
+
+    print(priv_pos, non_priv_pos)
+
+    print(f"Accuracy: {accuracy}")
+    print(f"Disparate impact: {max(priv_pos / non_priv_pos, non_priv_pos / priv_pos)}")
+
+
+if __name__ == '__main__':
+    main()
